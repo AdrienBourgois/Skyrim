24004e69491a924b1fae61097e74c9a7a0a4179d
Million Quentin <q.million@student.isartdigital.com>
2016-10-10 17:17:40 +0200

    Base class for characteristics and attributes

---
+++ Assets/Project/Script/Character/Attributes.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attributes : MonoBehaviour {
+
+    private int strength = 10;
+    private int constitution = 10;
+    private int intelligence = 10;
+    private int dexterity = 10;
+
+    
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+
+    #region Strength
+
+    public int Strength
+    { get { return strength; } set { strength = value; } }
+
+
+
+
+    #endregion
+
+    #region Constitution
+
+    public int Constitution
+    {
+        get { return constitution; } set { constitution = value; } }
+
+
+
+    #endregion
+
+    #region Intelligence
+
+    public int Intelligence
+    {
+        get { return intelligence; } set { intelligence = value; } }
+
+
+
+    #endregion
+
+    #region Dexterity
+
+    public int Dexterity
+    {
+        get { return dexterity; } set { dexterity = value; } }
+
+    #endregion
+
+
+}
---
+++ Assets/Project/Script/Character/CharacterStats.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStats : MonoBehaviour {
+
+    #region Stats
+    private Characteristics characteristics;
+    public Characteristics UnitCharacteristics
+    {
+        get { return characteristics; }
+    }
+
+    private Attributes attributes;
+    public Attributes UnitAttributes
+    {
+        get { return attributes; }
+    }
+
+    #endregion  
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void SetCaracteristics()
+    {
+        UnitCharacteristics.Weight = UnitAttributes.Strength * 10;
+        UnitCharacteristics.Attack *= UnitAttributes.Strength;
+
+    }
+}
