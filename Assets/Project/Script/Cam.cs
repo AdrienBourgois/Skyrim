b4f81acd34ed84e25295f73dea0d9cbf65464b25
Thomas <t.aubart@student.isartdigital.com>
2016-10-10 17:30:49 +0200

    Camera Now have a limit of rotation Up/Down - > small issues are still there

--- Assets/Project/Script/Cam.cs
+++ Assets/Project/Script/Cam.cs
@@ -3,20 +3,46 @@ using System.Collections;
 
 public class Cam : MonoBehaviour {
 
-    [SerializeField] float sensibility = 1f; 
+    [SerializeField] float YAngleMin = -70f;
+    [SerializeField] float YAngleMax = 70f;
 
-	void Start () {
-	
+    [SerializeField] float sensibility = 1f;
+
+    Transform target;
+
+    private float currRotX = 0f;
+
+	void Start ()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player)
+        {
+            target = player.transform;
+
+            transform.rotation = new Quaternion(target.forward.x, target.forward.y, target.forward.z, 0f);
+        }
+        else
+            Debug.Log("There is no Player :(");
 	}
 	
 
 	void Update ()
     {
-        //transform.position = target.position;
+        if (target)
+            transform.position = target.position; //+ Vector3.up;
 
         float rotY = Input.GetAxis("Mouse X") * sensibility * Time.deltaTime;
         float rotX = Input.GetAxis("Mouse Y") * sensibility * Time.deltaTime;
 
-        transform.localEulerAngles =  new Vector3(transform.localEulerAngles.x - rotX , transform.localEulerAngles.y + rotY, 0f);
+        Debug.Log(currRotX);
+
+        currRotX += rotX;
+        currRotX = Mathf.Clamp(currRotX, YAngleMin, YAngleMax);
+
+        if (YAngleMin < currRotX && currRotX < YAngleMax)
+        {
+            target.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x - rotX, transform.localEulerAngles.y + rotY, 0f);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x - rotX, transform.localEulerAngles.y + rotY, 0f);
+        }
     }
 }
