7d0018efbbf2e44ae91ac534f6e7b9370b86f8ba
Guillaume <g.newton@student.isartdigital.com>
2016-10-10 15:05:47 +0200

    Added ACharacter to files
    Added ACharacter that can't compile for implementation.

---
+++ Assets/Project/Script/Character/ACharacter.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Abstract class for every character in the game. An ACharacter has a UnitName and Base Stats as serialized fields.
+/// </summary>
+public abstract class ACharacter : MonoBehaviour
+{
+    #region Serialized Fields
+    [SerializeField]
+    private string unitName;
+    public string UnitName
+    {
+        get { return unitName; }
+        protected set { unitName = value; }
+    }
+
+    [SerializeField]
+    private int baseAttack;
+    [SerializeField]
+    private int baseDefense;
+    [SerializeField]
+    private int baseWeight;
+    [SerializeField]
+    private int baseHealth;
+    [SerializeField]
+    private int baseMana;
+    [SerializeField]
+    private int baseSpellPower;
+    [SerializeField]
+    private int basePrecision;
+    [SerializeField]
+    private int baseAttackSpeed;
+    #endregion
+
+    /* #region Stats & Inventory
+    private Characteristics characteristics;
+    public Characteristics UnitCharacteristics
+    {
+        get { return characteristics; }
+    }
+
+    private Attributes attributes;
+    public Attributes UnitAttributes
+    {
+        get { return attributes; }
+    }
+
+    private Inventory inventory;
+    public Inventory UnitInventory
+    {
+        get { return inventory; }
+    }
+    #endregion
+    */
+
+    protected virtual void Start()
+    {
+        /*
+        characteristics.Init(baseAttack, baseDefense,
+                             baseWeight, baseHealth,
+                             baseMana, baseSpellPower,
+                             basePrecision, baseAttackSpeed);
+                             */
+    }
+}
