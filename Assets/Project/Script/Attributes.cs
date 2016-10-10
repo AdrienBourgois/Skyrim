551c84b27a5a98f44987d92474a0987fe6acb09a
Million Quentin <q.million@student.isartdigital.com>
2016-10-10 14:11:36 +0200

    [WIP] Attributes and Caracteristics class

---
+++ Assets/Project/Script/Attributes.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attributes : MonoBehaviour {
+
+    private int strength;
+    private int constitution;
+    private int intelligence;
+    private int dexterity;
+
+    
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+
+    #region Strength
+
+    public int Strength
+    {
+        get
+        {
+            return strength;
+        }
+
+        set
+        {
+            strength = value;
+        }
+    }
+
+
+
+
+    #endregion
+
+    #region Constitution
+
+    public int Constitution
+    {
+        get
+        {
+            return constitution;
+        }
+
+        set
+        {
+            constitution = value;
+        }
+    }
+
+
+
+    #endregion
+
+    #region Intelligence
+
+    public int Intelligence
+    {
+        get
+        {
+            return intelligence;
+        }
+
+        set
+        {
+            intelligence = value;
+        }
+    }
+
+
+
+    #endregion
+
+    #region Dexterity
+
+    public int Dexterity
+    {
+        get
+        {
+            return dexterity;
+        }
+
+        set
+        {
+            dexterity = value;
+        }
+    }
+
+    #endregion
+}
