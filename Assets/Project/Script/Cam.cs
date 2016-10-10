4bdbf9f792db59b6c7f7adff5edbaa221a92c279
Thomas <t.aubart@student.isartdigital.com>
2016-10-10 16:08:10 +0200

    Basic Fps Camera implemented

---
+++ Assets/Project/Script/Cam.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cam : MonoBehaviour {
+
+    [SerializeField] float sensibility = 1f; 
+
+	void Start () {
+	
+	}
+	
+
+	void Update ()
+    {
+        //transform.position = target.position;
+
+        float rotY = Input.GetAxis("Mouse X") * sensibility * Time.deltaTime;
+        float rotX = Input.GetAxis("Mouse Y") * sensibility * Time.deltaTime;
+
+        transform.localEulerAngles =  new Vector3(transform.localEulerAngles.x - rotX , transform.localEulerAngles.y + rotY, 0f);
+    }
+}
