d33e0dc17bf295f7e6095cd91fdef0bb62a95381
Thomas <t.aubart@student.isartdigital.com>
2016-10-10 12:47:50 +0200

    Added GameManager

---
+++ Assets/Project/Script/Manager/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private GameManager instance;
+    public GameManager Instance
+    {
+        get
+        {
+            if (!instance)
+                instance = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+            return instance;
+        }
+    }
+
+    enum GameState
+    {
+        Intro = 0,
+        MainMenu,
+        InGame,
+        Death,
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
