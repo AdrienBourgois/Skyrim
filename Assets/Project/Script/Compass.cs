75d314d69347b34447d5a46122f33bcc2183a269
Thomas <t.aubart@student.isartdigital.com>
2016-10-12 17:31:04 +0200

    WIP arrow targeting exit or objectives ...

--- Assets/Project/Script/Compass.cs
+++ Assets/Project/Script/Compass.cs
@@ -20,30 +20,38 @@ public class Compass : MonoBehaviour {
         else
             player = player_gao.transform;
 
-        Transform compassCylinder = transform.FindChild("CompassCylinder");
-
-        needle = compassCylinder.FindChild("Needle");
-        arrow1 = compassCylinder.FindChild("Arrow1").gameObject;
-        arrow2 = compassCylinder.FindChild("Arrow2").gameObject;
+        needle = transform.FindChild("Needle");
+        arrow1 = transform.FindChild("CompassCylinder").FindChild("Arrow1").gameObject;
+        arrow2 = transform.FindChild("CompassCylinder").FindChild("Arrow2").gameObject;
     }
 
 
     void Update()
     {
         if (!CheckTarget())
+        {
+            DisableArrow();
             return;
+        }
 
         if (arrow1.GetComponent<SpriteRenderer>().enabled == false)
         {
+
             needle.position = player.position;
             needle.LookAt(target);
+            needle.forward = new Vector3(needle.forward.x, 0f, needle.forward.z);
 
+            Vector3 player_fwd_cpy = new Vector3(player.forward.x, 0f, player.forward.z);
+
+            float rotAngle = Vector3.Angle(player_fwd_cpy, needle.forward);
+
+            Debug.Log(rotAngle);
             arrow1.GetComponent<SpriteRenderer>().enabled = true;
             arrow2.GetComponent<SpriteRenderer>().enabled = true;
-            needle.forward = new Vector3(needle.forward.x, 0f, needle.forward.z);
-            arrow1.transform.position += needle.forward /1.9f;
-            arrow2.transform.position -= needle.forward /1.9f;
+            arrow1.transform.position = transform.position + player_fwd_cpy /2f;
+            arrow1.transform.RotateAround(transform.position, Vector3.up, -rotAngle);
         }
+        
     }
 
     bool CheckTarget()
