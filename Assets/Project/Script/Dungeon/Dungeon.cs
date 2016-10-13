cfdf324e8272f20906e7a3df15484c4feaa09b8c
Million Quentin <q.million@student.isartdigital.com>
2016-10-13 16:43:37 +0200

    Create dungeon script for procedural generation of dungeons

---
+++ Assets/Project/Script/Dungeon/Dungeon.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class Dungeon : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    Vector3 getRandomPointInCircle(float radius)
+    {
+        Vector3 pos = Vector3.zero;
+
+        float t = 2 * (float)Math.PI * UnityEngine.Random.Range(0f, radius);
+        float u = UnityEngine.Random.Range(0f, radius) + UnityEngine.Random.Range(0f, radius);
+        float r = 0f;
+
+        if (u > 1)
+            r = 2 - u;
+        else
+            r = u;
+
+        pos.x = (int)Mathf.Round(radius * r * (float)Math.Cos(t));
+        pos.y = 0;
+        pos.z = (int)Mathf.Round(radius * r * (float)Math.Sin(t));
+
+        return pos;
+    }
+}
