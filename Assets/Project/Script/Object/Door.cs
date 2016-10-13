99f971a9d2a6f07c8bbdbef7789f54a4ef8f5c67
Million Quentin <q.million@student.isartdigital.com>
2016-10-13 10:59:22 +0200

    Create door with animation but animation don't start when the player use the door, try to fix it

---
+++ Assets/Project/Script/Object/Door.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Animation))]
+public class Door : MonoBehaviour, IUsableObject
+{
+
+    private Animation anim = null;
+    private bool hasBeenOpen = false;
+
+
+    public void OnUse(ACharacter character)
+    {
+        print("OnUse");
+
+        if (hasBeenOpen == false)
+        {
+            print("test");
+            anim.Play("CloseDoor");
+            hasBeenOpen = true;
+        }
+        else if (hasBeenOpen == true)
+        {
+            anim.Play("CloseDoor");
+        }
+    }
+
+    void Start () {
+
+        anim = GetComponent<Animation>();
+    }
+	
+	void Update () {
+	
+	}
+}
