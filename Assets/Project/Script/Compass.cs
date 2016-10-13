6f6ef82f04891c66a800c2f7fa7451f21ab7ce1d
Thomas <t.aubart@student.isartdigital.com>
2016-10-13 12:13:55 +0200

    Compass scale nicely with screen

--- Assets/Project/Script/Compass.cs
+++ Assets/Project/Script/Compass.cs
@@ -6,6 +6,7 @@ public class Compass : MonoBehaviour {
     //Transform[] targets;
     Transform target;
     Transform player;
+
     Transform needle;
 
     Transform arrowRotator;
@@ -15,20 +16,14 @@ public class Compass : MonoBehaviour {
     void Start()
     {
         CheckTarget();
-
-        GameObject player_gao = GameObject.FindGameObjectWithTag("Player");
-        if (player_gao == null)
-            Debug.LogError("Compass.Start() - could not find player GameObject");
-        else
-            player = player_gao.transform;
-
+        FindPlayer();
+        
         needle = transform.FindChild("Needle");
 
         arrowRotator = transform.FindChild("ArrowRotator");
         arrow1 = arrowRotator.FindChild("Arrow1");
         arrow2 = arrowRotator.FindChild("Arrow2");
     }
-
 
     void Update()
     {
@@ -41,37 +36,60 @@ public class Compass : MonoBehaviour {
         needle.position = player.position;
         needle.LookAt(target);
 
+        UpdateArrow();
+
+    }
+
+    void UpdateArrow()
+    {
+        if (arrow1.GetComponent<SpriteRenderer>().enabled == false)
+            EnableArrow();
+
         needle.forward = new Vector3(needle.forward.x, 0f, needle.forward.z);
         Vector3 player_fwd_cpy = new Vector3(player.forward.x, 0f, player.forward.z);
 
+
         float rotAngle = Vector3.Angle(player_fwd_cpy, needle.forward);
 
+        //When target is on the rightside of the player
         if (Vector3.Angle(player.right, needle.forward) > 90f)
             rotAngle *= -1;
 
-
-        if (arrow1.GetComponent<SpriteRenderer>().enabled == false)
-        {
-            arrow1.GetComponent<SpriteRenderer>().enabled = true;
-            arrow2.GetComponent<SpriteRenderer>().enabled = true;
-            arrow1.transform.position = transform.FindChild("CompassCylinder").position + player_fwd_cpy / 1.9f;
-            arrow2.transform.position = transform.FindChild("CompassCylinder").position - player_fwd_cpy / 1.9f;
-            // Bug when player is looking up or down
-        }
-
-        arrowRotator.transform.eulerAngles = new Vector3(0f, -(rotAngle /2f), 0f);
+        arrowRotator.transform.eulerAngles = new Vector3(0f, -(rotAngle / 2f), 0f);
     }
 
+    void FindPlayer()
+    {
+        GameObject player_gao = GameObject.FindGameObjectWithTag("Player");
+
+        if (player_gao == null)
+            Debug.LogError("Compass.Start() - could not find player GameObject");
+        else
+            player = player_gao.transform;
+    }
     bool CheckTarget()
     {
         GameObject target_gao = GameObject.FindGameObjectWithTag("CompassTarget");
         if (target_gao == null)
+        {
+            Debug.LogError("Compass.CheckTarget() - any valid target");
             return false;
+        }
 
         target = target_gao.transform;
         return true;
     }
 
+    void EnableArrow()
+    {
+        Vector3 player_fwd_cpy = new Vector3(player.forward.x, 0f, player.forward.z);
+
+        arrow1.GetComponent<SpriteRenderer>().enabled = true;
+        arrow2.GetComponent<SpriteRenderer>().enabled = true;
+        arrow1.transform.position = transform.FindChild("CompassCylinder").position + player_fwd_cpy / 2f;
+        arrow2.transform.position = transform.FindChild("CompassCylinder").position - player_fwd_cpy / 2f;
+        // Bug when player is looking up or down
+    }
     void DisableArrow()
     {
         arrow1.GetComponent<SpriteRenderer>().enabled = false;
