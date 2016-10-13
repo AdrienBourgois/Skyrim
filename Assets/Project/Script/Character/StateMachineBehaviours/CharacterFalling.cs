61f8628d493fd59e4857ed5da8e8c456d6100119
Million Quentin <q.million@student.isartdigital.com>
2016-10-13 15:27:36 +0200

    Add the new Player with Animation and recover the new gui architecture

--- Assets/Project/Script/Character/StateMachineBehaviours/CharacterFalling.cs
+++ Assets/Project/Script/Character/StateMachineBehaviours/CharacterFalling.cs
@@ -9,7 +9,7 @@ public class CharacterFalling : ACharacterAnimatorBehaviour
         base.OnStateEnter(animator, stateInfo, layerIndex);
         finalTriggerValues = new CapsuleColliderCopy(new Vector3(0.0f, 0.9f, 0.0f), 0.3f, 1.9f);
         finalColliderValues = new CapsuleColliderCopy(new Vector3(-0.05f, 1.28f, 0.05f), 0.35f, 1.3f);
-        Debug.Log("ENTERED FALLING");
+
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
--- Assets/Project/Script/Compass.cs
+++ Assets/Project/Script/Compass.cs
@@ -55,7 +55,7 @@ public class Compass : MonoBehaviour {
         if (Vector3.Angle(player.right, needle.forward) > 90f)
             rotAngle *= -1;
 
-        Debug.Log("Update");
+
 
         arrowRotator.transform.eulerAngles = new Vector3(0f, -(rotAngle / 2f), 0f);
     }
