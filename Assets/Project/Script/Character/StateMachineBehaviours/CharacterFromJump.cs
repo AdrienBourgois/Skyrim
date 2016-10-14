e1022bf43ecfd45817daf9bc25b1365690c61453
Guillaume <g.newton@student.isartdigital.com>
2016-10-14 10:06:57 +0200

    Fixed bug in Controller
    Fixed bug where Landing animation would trigger but not player. Was multiplied by JumpEfficiency, but was initialized to 0.0f.

--- Assets/Project/Script/Character/StateMachineBehaviours/CharacterFromJump.cs
+++ Assets/Project/Script/Character/StateMachineBehaviours/CharacterFromJump.cs
@@ -18,9 +18,10 @@ public class CharacterFromJump : ACharacterAnimatorBehaviour
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetFloat("JumpEfficiency", 1.0f);
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
