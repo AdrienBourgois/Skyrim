9874de34e9fe70f9fe17518924d33671f94b7404
Thomas <t.aubart@student.isartdigital.com>
2016-10-28 10:35:10 +0200

    xp bar Debug

--- Assets/Project/Script/Character/ACharacterController.cs
+++ Assets/Project/Script/Character/ACharacterController.cs
@@ -238,7 +238,6 @@ public abstract class ACharacterController : APausableObject
 
     void OnDestroy()
     {
-        Debug.Log("controller death");
         GameManager.OnPause -= PutPause;
     }
 }
--- Assets/Project/Script/Character/Enemy/EnemyController.cs
+++ Assets/Project/Script/Character/Enemy/EnemyController.cs
@@ -63,8 +63,6 @@ public class EnemyController : ACharacterController
                 ControllerDrawSheathSword();
                 bIsAttacking = false;
             }
-            else
-                needUpdate = false;
 
             yield return new WaitForSeconds(0.5f);
         }
