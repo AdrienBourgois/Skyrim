485707aea2c32b04c85c4ffd99126d181039417a
Thomas <t.aubart@student.isartdigital.com>
2016-10-28 16:09:44 +0200

    debug bar

--- Assets/Project/Script/Gui/Bar/ManaBar.cs
+++ Assets/Project/Script/Gui/Bar/ManaBar.cs
@@ -10,13 +10,13 @@ public class ManaBar : Bar {
         Characteristics playerStats = player.CharacterStats.UnitCharacteristics;
         float manaRatio = playerStats.Mana / playerStats.MaxMana;
 
-        lastManaValue = playerStats.Mana;
-        lastMaxManaValue = playerStats.MaxMana;
-
         if (playerStats.Mana >= 0 && (lastManaValue != playerStats.Mana || lastMaxManaValue != playerStats.MaxMana))
         {
             bar.localScale = new Vector3(manaRatio, bar.localScale.y, bar.localScale.z);
             point.text = playerStats.Mana + " / " + playerStats.MaxMana.ToString("0");
         }
+
+        lastManaValue = playerStats.Mana;
+        lastMaxManaValue = playerStats.MaxMana;
     }
 }
